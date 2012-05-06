3ad1307c34390da294a04c2e5752559ec374b3b9
Andersen Fan <as181920@hotmail.com>
2012-05-06 22:18:16 +0800

    全部重构

--- fetch_and_play_video.rb
+++ fetch_and_play_video.rb
@@ -4,81 +4,85 @@ require "open-uri"
 require "mail"
 require "gtk2"
 
-Mail.defaults do
-  delivery_method :smtp, {
-    address: "smtp.gmail.com",
-    user_name: "xinye181920",
-    password: "xinye.181920",
-    authentication: "plain",
-  }
-end
+include "notify_mail"
 
-def mail_notification(msg)
-  mail = Mail.deliver do
-    to "13816956163@139.com"
-    from "xinye181920@gmail.com"
-    subject "xinye video program error!"
-    text_part do
-      body msg
-    end
-  end
-  puts "notification mail sent!"
-rescue => e
-  puts e
+current_path = File.dirname(__FILE__)
+
+def video_in_folder(path)
+  Dir.glob(File.join(current_path,"*")).first.split("/").last
 end
 
 def net_connected?
-  begin
-    true if open("http://www.xinyegroup.com/dalaoju.html")
-  rescue => e
-    puts e
-    mail_notification e.to_s
-  end
+  true if open("http://www.xinyegroup.com/dalaoju.html")
+rescue => e
+  puts e
+  mail_notification e.to_s
 end
 
 def clean_old_files
   puts "clean all old files"
-  Dir.glob("*").each do |f|
-    system "rm -rf #{f}" unless f == "fetch_and_play_video.rb"
+  Dir.glob(File.join(current_path,"*")).each do |f|
+    puts "deleted file: #{f}" if File.delete f
   end
+rescue => e
+  puts e
+  mail_notification e.to_s
 end
 
-$last_video = nil
-$current_video = nil
+def play_video(video)
+  system "mplayer -fs -loop 0 #{video} &"
+rescue => e
+  puts e
+  mail_notification e.to_s
+end
 
-clean_old_files
+def new_video
+  loop do
+    if net_connected?
+      break
+    else
+      system "zenity --error --text='网络连接异常！'"
+    end
+  end
+  video_url = open("http://www.xinyegroup.com/dalaoju.html").read.strip
+  video_name = video_url.split("/").last
+  if video_name == video_in_folder then
+    false
+  else
+    video_url
+  end
+rescue => e
+  puts e
+  mail_notification e.to_s
+end
+
+play_video File.join(current_path,"downloads",video_in_folder)
 
 loop do
   begin
-    if net_connected?
-      puts video_path = open("http://www.xinyegroup.com/dalaoju.html").read
-      #puts video_path = "http://younoter.com/assets/rails.png"
-      puts $current_video = video_name = video_path.strip.split("/").last
-      if $last_video and $current_video == $last_video
-        puts "无更新."
-      else
-        puts "已更新."
-        system "rm -rf #{$last_video}"
-        system "zenity --info --text='找到视频，正在努力下载，请确保网络正常...' &"
-        download = system "wget -c #{video_path}"
-        system "killall zenity"
-        if download then
-          $last_video = $current_video
-          system "killall mplayer"
-          system "mplayer -fs -loop 0 #{video_name} &"
-        else
-          puts "error"
-          raise "restart mplayer error!"
-        end
-      end
+  puts "检查是否有更新--->"
+  if video_path = new_video
+    puts "updated..."
+    system "zenity --info --text='找到视频，正在努力下载，请确保网络正常...' &"
+    clean_old_files
+    download = system "wget -c #{video_path}"
+    if download then
+      system "killall zenity"
+      system "killall mplayer"
+      system "mplayer -fs -loop 0 #{video_in_folder} &"
     else
-      #system "xmessage 网络连接异常... -timeout 3"
-      system "zenity --error --text='网络连接异常...' --timeout=3"
+      puts "download error!"
+      raise "download error!"
     end
+  else
+    puts "no update."
+  end
+  sleep 3
   rescue => e
     puts e
     mail_notification e.to_s
+    sleep 30
   end
-  sleep 3
-  puts "检查是否有更新--->"
 end
+
+
