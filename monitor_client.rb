0d615f3b48c0b1cdfacac33a205d6e5a755f938b
Andersen Fan <as181920@hotmail.com>
2012-05-20 17:34:44 +0800

    增加运行状态监控的客户端和服务器端

---
+++ monitor_client.rb
@@ -0,0 +1,56 @@
+# encoding: utf-8
+require "open-uri"
+require "yaml"
+
+def location
+  YAML.load(File.read(File.join(File.dirname(__FILE__),"config/location.yml")))["loc"]
+rescue => e
+  return e
+end
+
+def video_in_folder
+  video_path = Dir.glob(File.join(File.dirname(__FILE__),"/downloads/*")).first
+  if video_path
+    video_path.split("/").last
+  else
+    nil
+  end
+rescue => e
+  return e
+end
+
+def internet_ip
+  (open('http://checkip.dyndns.org/').read.scan(/(\d+\.\d+\.\d+\.\d+)/))[0][0]
+rescue => e
+  return e
+end
+
+def video_on_net
+  open("http://www.xinyegroup.com/dalaoju/s.aspx?id=#{location}").read.strip
+rescue => e
+  return e
+end
+
+def process
+  %x[ps aux|grep vlc|grep down] + %x[ps aux|grep ruby|grep rb]
+rescue => e
+  return e
+end
+
+def performance
+  %x[uptime] +"\n"+ %x[iostat] + %x[free -ml] + %x[sensors]
+rescue => e
+  return e
+end
+
+loop do
+  begin
+    print "."
+    status = {location: location, ip: internet_ip, video_in_folder: video_in_folder, video_on_net: video_on_net, performance: performance, process: process}
+    Net::HTTP.post_form URI.parse("http://localhost:4567/dalaoju"), status
+  rescue => e
+    puts e
+    sleep 30
+  end
+  sleep 3
+end
---
+++ monitor_server.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require "sinatra"
+require "mongo"
+
+MongoConn = Mongo::Connection.new "localhost", 27017
+DB = MongoConn.db "uplayer"
+Coll = DB.collection "status"
+
+get "/" do
+  "<br/> <br/> <br/> 私人领地，闲人莫入！ <br/><br/> <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ------Andersen Fan(as181920@hotmail.com)"
+end
+
+
+get "/dalaoju" do
+  @logs = Coll.find.sort(["created_at","descending"]).skip([(params[:page].to_i-1)*10,0].max).limit(20)
+  @cnt_pages = (Coll.find.count.to_f / 10).ceil
+  erb :log, locals: {page: params[:page], cnt_pages: @cnt_pages, logs: @logs} 
+end
+
+post "/dalaoju" do
+  params[:created_at] = Time.now
+  Coll.insert params
+  "successfully logged @#{Time.now}"
+end
+
+private
+
+
