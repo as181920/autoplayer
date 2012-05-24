5328586a124224a4897197eeb32222bcf2a54bc0
Andersen Fan <as181920@hotmail.com>
2012-05-24 16:14:01 +0800

    cancel default_16:9

--- fetch_and_play_video.rb
+++ fetch_and_play_video.rb
@@ -79,7 +79,7 @@ end
 
 def play_video(video)
   #system "mplayer -fs -loop 0 #{video} &"
-  system "cvlc -fL --one-instance --no-video-title-show --no-video-on-top --aspect-ratio=16:9 #{video} -d"
+  system "cvlc -fL --one-instance --no-video-title-show --no-video-on-top #{video} -d"
 rescue => e
   puts e
   mail_notification e.to_s
