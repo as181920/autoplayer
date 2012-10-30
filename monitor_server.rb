7f22801bd81a7dc2d5415cc0cb1af67413bec5d7
Andersen Fan <as181920@hotmail.com>
2012-10-30 16:59:33 +0800

    more simpler

--- monitor_server.rb
+++ monitor_server.rb
@@ -23,6 +23,12 @@ get "/dlj" do
   erb :dlj, locals: {page: params[:page], logs: @logs} 
 end
 
+get "/dl" do
+  @per_page = 100
+  @logs = Coll.find.sort(["created_at","descending"]).skip([(params[:page].to_i-1)*@per_page,0].max).limit(@per_page)
+  erb :dl, locals: {page: params[:page], logs: @logs} 
+end
+
 post "/dalaoju" do
   params[:created_at] = Time.now
   Coll.insert params
