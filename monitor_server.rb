37ccc0372f6c4fa3d5b3a091abf62bcee3119c82
Andersen Fan <as181920@hotmail.com>
2012-10-30 16:30:27 +0800

    add simple show page

--- monitor_server.rb
+++ monitor_server.rb
@@ -17,6 +17,12 @@ get "/dalaoju" do
   erb :log, locals: {page: params[:page], cnt_pages: @cnt_pages, logs: @logs} 
 end
 
+get "/dlj" do
+  @logs = Coll.find.sort(["created_at","descending"]).skip([(params[:page].to_i-1)*10,0].max).limit(20)
+  @cnt_pages = (Coll.find.count.to_f / 10).ceil
+  erb :dlj, locals: {page: params[:page], cnt_pages: @cnt_pages, logs: @logs} 
+end
+
 post "/dalaoju" do
   params[:created_at] = Time.now
   Coll.insert params
